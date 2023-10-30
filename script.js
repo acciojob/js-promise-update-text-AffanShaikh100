a5ec37f66b32bc865190e23ec3593841ab945f0c
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-10-30 12:04:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+window.onload=()=>{
+	let div = document.getElementById("output");
+	const promise = new promise(function(resolve,reject)){
+		setTimeout(()=>{
+			resolve("Hello, world!");
+		},1000);
+	}
+
+	promise.then((value)=>{
+		div.innerHTML = value;
+	})	
+		
+}
